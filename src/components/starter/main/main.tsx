2ed020374471824000c44203526a5d00778bc792
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2023-08-12 13:38:21 +0330

    Responsive Design(view)

--- src/components/starter/main/main.tsx
+++ src/components/starter/main/main.tsx
@@ -7,23 +7,19 @@ export default component$(() => {
   useStylesScoped$(styles);
   return (
     <div class=" flex justify-center mt-20">
-      <div class=" border-slate-200 bg-amber-50	w-2/5 h-auto rounded-lg drop-shadow-xl ">
+      <div class="border">
         <div class="row">
-          <h1 class="text-4xl text-violet-500 mt-5">User Management</h1>
+          <h1 class="font">User Management</h1>
         </div>
         <div class="row">
           <div class=" flex justify-center ">
             <Link class="no-underline" href="/view">
-              <button class="rounded-lg	 w-44 h-36 bg-violet-400	text-white m-8">
-                view
-              </button>
+              <button class="btn-view">view</button>
             </Link>
           </div>
           <div class=" flex justify-center">
             <Link class="no-underline" href="/add">
-              <button class="rounded-lg	 w-44 h-36 bg-violet-400 text-white m-8	">
-                add/edit
-              </button>
+              <button class="btn-view">add/edit</button>
             </Link>
           </div>
         </div>
